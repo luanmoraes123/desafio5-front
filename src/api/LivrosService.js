13876ed62e4bc73a1b753ba8469437c937996764
luan moraes <luan.moraees21@gmail.com>
2023-09-13 11:08:29 -0300

    projeto pronto

---
+++ src/api/LivrosService.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const BASE_URL = "https://api-biblioteca-3yqx.onrender.com"
+
+export class LivrosService {
+    static getLivros() {
+        return axios.get(BASE_URL + '/livros');
+    }
+
+    static getLivro(id) {
+        return axios.get(`${BASE_URL}/livros/${id}`);
+    }
+
+    static createLivro(body) {
+        return axios.post(`${BASE_URL}/livros`, body);
+    }
+
+    static updateLivro(id, body) {
+        return axios.put(`${BASE_URL}/livros/${id}`, body);
+    }
+
+    static deleteLivro(id) {
+        return axios.delete(`${BASE_URL}/livros/${id}`);
+    }
+
+}
---
+++ src/views/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Header from "../../components/Header/Header";
+import "./index.scss";
+
+const Home = () => {
+  return (
+    <div className='home'>      
+      <Header/>
+      <h1>Biblioteca Central Online - Livros</h1>
+    </div>
+  )
+}
+
+export default Home
